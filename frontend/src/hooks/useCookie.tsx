99596ce76f2bccc0c44772ea9f33121f72ff45c1
francids <francisco.mc.ids@gmail.com>
2025-05-13 18:13:03 -0400

    ui(frontend): Add secure and expiration options to default cookie setting in useCookie hook

--- frontend/src/hooks/useCookie.tsx
+++ frontend/src/hooks/useCookie.tsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import Cookies from "js-cookie";
 
-export default function useCookie<T>(keyName: string, defaultValue?: T) {
+const cookieOptions: Cookies.CookieAttributes = {
+  expires: 30,
+  secure: true,
+};
+
+export default function useCookie<T>(
+  keyName: string,
+  defaultValue?: T,
+  options: Cookies.CookieAttributes = cookieOptions
+): [T | undefined, (value: T) => void] {
   const [storedValue, setStoredValue] = useState<T | undefined>(() => {
     try {
       const value = Cookies.get(keyName);
       if (value) {
         return JSON.parse(value) as T;
       } else {
-        Cookies.set(keyName, JSON.stringify(defaultValue));
+        Cookies.set(keyName, JSON.stringify(defaultValue), options);
         return defaultValue;
       }
     } catch (err: unknown) {
@@ -19,7 +28,7 @@ export default function useCookie<T>(keyName: string, defaultValue?: T) {
 
   const setValue = (newValue: T) => {
     try {
-      Cookies.set(keyName, JSON.stringify(newValue));
+      Cookies.set(keyName, JSON.stringify(newValue), options);
     } catch (err: unknown) {
       console.error("Error setting cookie:", err);
     }
