8bafc84ac3222d1613cf9961b7f81fb2155f88d8
francids <francisco.mc.ids@gmail.com>
2025-05-14 01:54:46 -0400

    backend: Add validation annotations to RegisterUserDto fields

--- backend/src/main/java/com/francids/escruta/backend/dtos/RegisterUserDto.java
+++ backend/src/main/java/com/francids/escruta/backend/dtos/RegisterUserDto.java
@@ -1,6 +1,9 @@
 package com.francids.escruta.backend.dtos;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +11,14 @@ import lombok.Setter;
 @Setter
 public class RegisterUserDto {
     @NotBlank
+    @Email
     private String email;
+
     @NotBlank
+    @Size(min = 8)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$", message = "must contain at least one lowercase letter, one uppercase letter and one number")
     private String password;
+
     @NotBlank
     private String fullName;
 }
