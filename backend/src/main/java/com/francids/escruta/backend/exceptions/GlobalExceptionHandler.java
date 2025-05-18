5899933ccc695981afd55afd950214c291b9926e
francids <francisco.mc.ids@gmail.com>
2025-05-17 23:24:03 -0400

    backend: Handle HttpMessageNotReadableException in GlobalExceptionHandler
    Added specific handling for HttpMessageNotReadableException to return a 400 status with a detailed error description for invalid request bodies. Adjusted logging level from error to debug for improved log clarity during debugging.

--- backend/src/main/java/com/francids/escruta/backend/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/francids/escruta/backend/exceptions/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -23,12 +24,17 @@ public class GlobalExceptionHandler {
     public ProblemDetail handleSecurityException(Exception exception) {
         ProblemDetail errorDetail = null;
 
-        logger.error(exception.getMessage(), exception);
+        logger.debug(exception.getMessage(), exception);
 
         if (exception instanceof BadCredentialsException) {
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
             errorDetail.setProperty("description", "The username or password is incorrect");
             return errorDetail;
+        }
+
+        if (exception instanceof HttpMessageNotReadableException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400), exception.getMessage());
+            errorDetail.setProperty("description", "The request body is invalid");
         }
 
         if (exception instanceof AccountStatusException) {
