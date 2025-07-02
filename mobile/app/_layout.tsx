f72a0d91e9ed5f7019dabb047229fabcc576fbba
Francisco Mesa <francisco.mc.ids@gmail.com>
2025-07-01 23:16:33 -0400

    ui(mobile): Fix Status Bar color

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,24 +3,25 @@ import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: {
-          backgroundColor: "#121212",
-        },
-        animation: "fade_from_bottom",
-        animationDuration: 300,
-      }}
-    >
+    <>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {
+            backgroundColor: "#121212",
+          },
+          animation: "fade_from_bottom",
+          animationDuration: 300,
+        }}
+      >
+        {/* Landing/Auth Routes */}
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+
+        {/* App Routes - Nested */}
+        <Stack.Screen name="(app)" />
+      </Stack>
       <StatusBar style="light" />
-
-      {/* Landing/Auth Routes */}
-      <Stack.Screen name="index" />
-      <Stack.Screen name="login" />
-
-      {/* App Routes - Nested */}
-      <Stack.Screen name="(app)" />
-    </Stack>
+    </>
   );
 }
