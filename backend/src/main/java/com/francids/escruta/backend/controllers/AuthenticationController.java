7b35ac85c0ae4800282afc1e72758ae743c48de2
Francisco Mesa <francisco.mc.ids@gmail.com>
2025-09-10 19:57:56 -0400

    refactor(auth): Simplify AuthenticationController by using Lombok for constructor injection

--- backend/src/main/java/com/francids/escruta/backend/controllers/AuthenticationController.java
+++ backend/src/main/java/com/francids/escruta/backend/controllers/AuthenticationController.java
@@ -1,33 +1,25 @@
 package com.francids.escruta.backend.controllers;
 
-import com.francids.escruta.backend.dtos.BasicUser;
 import com.francids.escruta.backend.dtos.LoginResponse;
 import com.francids.escruta.backend.dtos.LoginUserDto;
 import com.francids.escruta.backend.dtos.RegisterUserDto;
 import com.francids.escruta.backend.entities.User;
 import com.francids.escruta.backend.services.AuthenticationService;
 import com.francids.escruta.backend.services.JwtService;
-import com.francids.escruta.backend.services.UserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthenticationController {
     private final JwtService jwtService;
     private final AuthenticationService authenticationService;
-    private final UserService userService;
-
-    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService, UserService userService) {
-        this.jwtService = jwtService;
-        this.authenticationService = authenticationService;
-        this.userService = userService;
-    }
 
     @PostMapping("/register")
     public ResponseEntity<LoginResponse> register(@Valid @RequestBody RegisterUserDto registerUserDto) {
