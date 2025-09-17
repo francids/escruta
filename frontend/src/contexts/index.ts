2b8c4a272be1013eb8dc95ab4a615b86c0b9b40b
Francisco Mesa <francisco.mc.ids@gmail.com>
2025-09-17 02:14:26 -0400

    fix: Expand ToastContext exports to include ToastContextType

--- frontend/src/contexts/index.ts
+++ frontend/src/contexts/index.ts
@@ -1,4 +1,8 @@
 export { default as AuthContext } from "./AuthContext";
 export { default as ModalContext } from "./ModalContext";
 export { default as ThemeContext, ThemeOptions } from "./ThemeContext";
-export { default as ToastContext, type ToastData } from "./ToastContext";
+export {
+  default as ToastContext,
+  type ToastData,
+  type ToastContextType,
+} from "./ToastContext";
