0620a57dba40153ddba12a028a30d4dfc382452b
Francisco Mesa <francisco.mc.ids@gmail.com>
2025-09-19 19:54:28 -0400

    docs: Update DocsLayout to include SimpleBackground and improve heading styles

--- frontend/src/docs/DocsLayout.tsx
+++ frontend/src/docs/DocsLayout.tsx
@@ -5,6 +5,7 @@ import { Outlet } from "react-router";
 import NavSidePanel from "./components/NavSidePanel";
 import NavTopBar from "./components/NavTopBar";
 import "./docs.css";
+import SimpleBackground from "@/landing/components/backgrounds/SimpleBackground";
 
 export default function DocsLayout() {
   const [title, setTitle] = useState("");
@@ -15,6 +16,7 @@ export default function DocsLayout() {
     <DocsContext.Provider
       value={{ title, setTitle, sidebarOpen, setSidebarOpen }}
     >
+      <SimpleBackground className="opacity-30" />
       <div className="h-screen flex flex-col">
         <NavTopBar />
         <div className="flex flex-grow overflow-hidden">
@@ -34,7 +36,7 @@ export default function DocsLayout() {
                 >
                   <path d="M20 22H4C3.44772 22 3 21.5523 3 21V3C3 2.44772 3.44772 2 4 2H20C20.5523 2 21 2.44772 21 3V21C21 21.5523 20.5523 22 20 22ZM19 20V4H5V20H19ZM7 6H11V10H7V6ZM7 12H17V14H7V12ZM7 16H17V18H7V16ZM13 7H17V9H13V7Z"></path>
                 </svg>
-                {title}
+                {title || "Documentation"}
               </span>
               <Outlet />
             </div>
