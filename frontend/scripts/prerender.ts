44c07a42a07e3c1c6fe1d36692b1fcdd9a510be6
Francisco Mesa <francisco.mc.ids@gmail.com>
2025-09-22 23:17:03 -0400

    frontend: Add root route to prerendering script

--- frontend/scripts/prerender.ts
+++ frontend/scripts/prerender.ts
@@ -9,6 +9,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const routes = [
+  "/",
   "/docs",
   "/docs/features/audio-summary",
   "/docs/features/flashcards",
