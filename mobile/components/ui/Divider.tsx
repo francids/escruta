328d71bd7ad5e241f37eb739a884ec1eebb1c1ea
Francisco Mesa <francisco.mc.ids@gmail.com>
2025-10-01 18:03:11 -0400

    ui(mobile): Update Divider component to use darker background color for improved contrast

--- mobile/components/ui/Divider.tsx
+++ mobile/components/ui/Divider.tsx
@@ -14,27 +14,29 @@ export default function Divider({
 }: DividerProps) {
   if (orientation === "vertical") {
     return (
-      <View style={[tw`h-full w-px bg-gray-100/60 dark:bg-gray-600`, style]} />
+      <View
+        style={[tw`h-full w-px bg-gray-100/60 dark:bg-gray-800/60`, style]}
+      />
     );
   }
 
   if (label) {
     return (
       <View style={[tw`flex-row items-center py-4`, style]}>
-        <View style={tw`flex-1 h-px bg-gray-100/60 dark:bg-gray-600`} />
+        <View style={tw`flex-1 h-px bg-gray-100/60 dark:bg-gray-800/60`} />
         <Text
           style={tw`mx-4 text-sm text-gray-600 dark:text-gray-300 font-sans`}
         >
           {label}
         </Text>
-        <View style={tw`flex-1 h-px bg-gray-100/60 dark:bg-gray-600`} />
+        <View style={tw`flex-1 h-px bg-gray-100/60 dark:bg-gray-800/60`} />
       </View>
     );
   }
 
   return (
     <View
-      style={[tw`h-px bg-gray-100/60 dark:bg-gray-600 my-6 w-full`, style]}
+      style={[tw`h-px bg-gray-100/60 dark:bg-gray-800/60 my-6 w-full`, style]}
     />
   );
 }
