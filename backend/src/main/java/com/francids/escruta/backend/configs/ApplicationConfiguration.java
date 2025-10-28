60bac1c99280c383ede513e908b09709e060b047
Francisco Mesa <francisco.mc.ids@gmail.com>
2025-10-28 15:53:22 -0400

    backend: Remove AsyncConfiguration and enable Async in ApplicationConfiguration

--- backend/src/main/java/com/francids/escruta/backend/configs/ApplicationConfiguration.java
+++ backend/src/main/java/com/francids/escruta/backend/configs/ApplicationConfiguration.java
@@ -3,6 +3,7 @@ package com.francids.escruta.backend.configs;
 import com.francids.escruta.backend.repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,6 +11,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableAsync
 public class ApplicationConfiguration {
     private final UserRepository userRepository;
 
