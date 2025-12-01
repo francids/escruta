f2dd59b5460cfb81f25caa8ff10901aae172432c
Francisco Mesa <francisco.mc.ids@gmail.com>
2025-11-30 20:54:29 -0400

    ui: Remove page transition animation

--- frontend/src/app/layouts/AppLayout.tsx
+++ frontend/src/app/layouts/AppLayout.tsx
@@ -1,15 +1,12 @@
 import { useEffect } from "react";
-import { Outlet, useLocation } from "react-router";
-import { AnimatePresence } from "motion/react";
+import { Outlet } from "react-router";
 import { useAuth, useCookie } from "@/hooks";
 import type { User } from "@/interfaces";
-import PageTransition from "@/shared/PageTransition";
 import SideMenu from "../components/SideMenu";
 
 export default function AppLayout() {
   const { currentUser, fetchUserData } = useAuth();
   const [, setUserData] = useCookie<User | null>("user", null);
-  const location = useLocation();
 
   useEffect(() => {
     if (currentUser) {
@@ -25,11 +22,7 @@ export default function AppLayout() {
     <div className="flex h-screen bg-white text-black dark:bg-black dark:text-white select-none">
       <SideMenu />
       <div className="flex-grow overflow-hidden">
-        <AnimatePresence mode="wait">
-          <PageTransition key={location.pathname}>
-            <Outlet />
-          </PageTransition>
-        </AnimatePresence>
+        <Outlet />
       </div>
     </div>
   );
